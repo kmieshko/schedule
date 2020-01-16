83aad0043a031dc00781f0e92328b31bde9c635d
kmieshko <meshkov.kirill@gmail.com>
2020-01-16 15:45:28 -0500

    with db

---
+++ database/migrations/2020_01_16_083501_create_schedules_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSchedulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('id_employee');
+            $table->tinyInteger('is_manager');
+            $table->string('id_week');
+            $table->tinyInteger('monday');
+            $table->tinyInteger('tuesday');
+            $table->tinyInteger('wednesday');
+            $table->tinyInteger('thursday');
+            $table->tinyInteger('friday');
+            $table->tinyInteger('saturday');
+            $table->tinyInteger('is_done');
+            $table->dateTime('date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedules');
+    }
+}
